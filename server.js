f21232a2bba8c8b932ce616cf36ac026089f8c4d
thewallen <matthew.jang@eagles.oc.edu>
2025-02-06 16:25:49 -0600

    Backend Template Code #1

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+const db = require("./app/models");
+
+db.sequelize.sync();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// Import and use all routes
+// require("./app/routes/auth.routes")(app);
+// require("./app/routes/user.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3100;
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+}
+
+module.exports = app;
