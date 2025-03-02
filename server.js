4e323ccd4a46ccf8d799c325fb92d765ce1256a2
matthew-jang <matthew.jang@eagles.oc.edu>
2025-03-02 16:37:40 -0600

    All routes included

--- server.js
+++ server.js
@@ -29,13 +29,21 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-// Import and use all routes
+/*** Importing Routes ***/
+// core routes
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
-require("./app/routes/clifton_strengths.routes")(app);
-require("./app/routes/reward.routes")(app); 
-require("./app/routes/event.routes")(app);
 
+// Close user info
+require("./app/routes/student.routes")(app);
+require("./app/routes/badge.routes")(app);
+require("./app/routes/major.routes")(app);
+require("./app/routes/clifton_strengths.routes")(app);
+
+// Tasks, Experiences, Points, Rewards
+require("./app/routes/checklist_item.routes")(app);
+require("./app/routes/event.routes")(app);
+require("./app/routes/reward.routes")(app); 
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3100;
