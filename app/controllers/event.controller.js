eddf12d9b0724c04f9810ae035d4b2221aa5704d
matthew-jang <matthew.jang@eagles.oc.edu>
2025-03-06 09:51:25 -0600

    events controller

--- app/controllers/event.controller.js
+++ app/controllers/event.controller.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const Event = db.Event;
+const Event = db.event;
 
 // Create a new event
 exports.createEvent = async (req, res) => {
@@ -13,14 +13,21 @@ exports.createEvent = async (req, res) => {
 };
 
 // Get all events
-exports.getAllEvents = async (req, res) => {
-  try {
-    const events = await Event.findAll();
-    res.status(200).json(events);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+exports.getAllEvents = (req, res) => {
+  const id = req.query.id;
+  const condition = id ? { event_id: { [Op.like]: `%${id}%` } } : null;
+
+  Event.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message
+      });
+    });
 };
+
 
 // Get event by ID
 exports.getEventById = async (req, res) => {
