c0e87ba9cc8e7672d2016f10794d73de0579357a
Jacob Honaker <83312842+JakePath21@users.noreply.github.com>
2025-03-27 19:11:40 -0500

    allowing sequilize to make timestamps

--- app/models/event.model.js
+++ app/models/event.model.js
@@ -1,42 +1,39 @@
 module.exports = (sequelize, DataTypes) => {
-    const Event = sequelize.define("event", {
-      event_id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      event_type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      start_time: {
-        type: DataTypes.TIME,
-        allowNull: false,
-      },
-      end_time: {
-        type: DataTypes.TIME,
-        allowNull: false,
-      },
-      location: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    },{
-      timestamps: false,
-    });
-  
-    return Event;
-  };
-  
+  const Event = sequelize.define("event", {
+    event_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    event_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    start_time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    end_time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
+  });
+
+  return Event;
+};
